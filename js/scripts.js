6c72860771e49b2804f388c6f9b71f244b92babd
Jaye-Cormier <echoesatdawn@gmail.com>
2023-01-19 12:12:13 -0500

    Create scripts.js
    New JS file. Features a greeting alert and my favorite pokemon

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+alert('Welcome to Galar! Home of the Dynamax and Gigantamax phenomenon!');
+let favoritePokemon = 'Sobble';
+document.write('My favorite Galar Region pokemon is ' + favoritePokemon);

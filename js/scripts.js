ba617d19a5f65264c5de83404e902d3680f8b402
Jaye-Cormier <echoesatdawn@gmail.com>
2023-01-19 14:33:48 -0500

    Update scripts.js
    Added array of 9 pokemon with name, type and weakness
    Removed previous test code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,47 @@
-alert('Welcome to Galar! Home of the Dynamax and Gigantamax phenomenon!');
-let favoritePokemon = 'Sobble';
-document.write('My favorite Galar Region pokemon is ' + favoritePokemon);
+GalarPokemonList =[
+    {
+        name: 'Grookey',
+        types: ['grass'],
+        weaknesses: ['fire']
+    },
+    {
+        name: 'Thwackey',
+        types: ['grass'],
+        weaknesses: ['fire']
+    },
+    {
+        name: 'Rillaboom',
+        types: ['grass'],
+        weaknesses: ['fire']
+    },
+    {
+        name: 'Scorbunny',
+        types: ['fire'],
+        weaknesses: ["water"]
+    },
+    {
+        name: 'Raboot',
+        types: ['fire'],
+        weaknesses: ['water']
+    },
+    {
+        name: 'Cinderace',
+        types: ['fire'],
+        weaknesses: ['water']
+    },
+    {
+        name: 'Sobble',
+        types: ['water'],
+        weaknesses: ['grass']
+    },
+    {
+        name: 'Drizzile',
+        types: ['water'],
+        weaknesses: ['grass']
+    },
+    {
+        name: 'Inteleon',
+        types: ['water'],
+        weaknesses: ['grass']
+    }
+];

ae3fad5f58abcef1617e0282b480af1cc6f833e8
Jaye-Cormier <echoesatdawn@gmail.com>
2023-01-20 11:10:07 -0500

    Update scripts.js
    added species and height to each object

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,86 @@
 GalarPokemonList =[
     {
         name: 'Grookey',
+        species: 'Chimp Pokemon',
         types: ['grass'],
-        weaknesses: ['fire']
+        weaknesses: ['fire'],
+        height: .3
     },
     {
         name: 'Thwackey',
+        species: 'Beat Pokemon',
         types: ['grass'],
-        weaknesses: ['fire']
+        weaknesses: ['fire'],
+        height: .7
     },
     {
         name: 'Rillaboom',
+        species: 'Drummer Pokemon',
         types: ['grass'],
-        weaknesses: ['fire']
+        weaknesses: ['fire'],
+        height: 2.1
     },
     {
         name: 'Scorbunny',
+        species: 'Rabbit Pokemon',
         types: ['fire'],
-        weaknesses: ["water"]
+        weaknesses: ["water"],
+        height: .3
     },
     {
         name: 'Raboot',
+        species: 'Rabbit Pokemon',
         types: ['fire'],
-        weaknesses: ['water']
+        weaknesses: ['water'],
+        height: .6
     },
     {
         name: 'Cinderace',
+        species: 'Striker Pokemon',
         types: ['fire'],
-        weaknesses: ['water']
+        weaknesses: ['water'],
+        height: 1.4
     },
     {
         name: 'Sobble',
+        species: 'Water Lizard Pokemon',
         types: ['water'],
-        weaknesses: ['grass']
+        weaknesses: ['grass'],
+        height: .3
     },
     {
         name: 'Drizzile',
+        species: 'Water Lizard Pokemon',
         types: ['water'],
-        weaknesses: ['grass']
+        weaknesses: ['grass'],
+        height: .7
     },
     {
         name: 'Inteleon',
+        species: 'Secret Agent Pokemon',
         types: ['water'],
-        weaknesses: ['grass']
+        weaknesses: ['grass'],
+        height: 1.9
     },
     {
         name: 'Blipbug',
+        species: 'Larva Pokemon',
         types: ['bug'],
-        weaknesses: ['fire', 'flying', 'rock']
+        weaknesses: ['fire', 'flying', 'rock'],
+        height: .4
     },
     {
         name: 'Dottler',
+        species: 'Radome Pokemon',
         types: ['bug', 'psychic'],
-        weaknesses: ['ghost', 'fire', 'flying', 'dark', 'rock', 'bug']
+        weaknesses: ['ghost', 'fire', 'flying', 'dark', 'rock', 'bug'],
+        height: .4
     },
     {
         name: 'Orbeetle',
+        species: 'Seven Spot Pokemon',
         types: ['bug', 'psychic'],
-        weaknesses: ['ghost', 'fire', 'flying', 'dark', 'rock', 'bug']
+        weaknesses: ['ghost', 'fire', 'flying', 'dark', 'rock', 'bug'],
+        height: .4
     }
 ];
